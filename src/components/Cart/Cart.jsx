3451e30a7e7b36a5f58e43fb216c882ac7ba8ce4
asmraihan <asmraihanctg14@gmail.com>
2023-03-28 06:47:39 +0600

    show data frm cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,20 +4,22 @@ import './Cart.css'
 const Cart = ({cart}) => { /* op3 */
     // const cart = props.cart  /* op1 */
     // const {cart} = props /* opt 2 */
-    // console.log(cart)
+    console.log(cart)
     let totalPrice = 0;
     let totalShipping = 0;
-
+    let quantity = 0;
     for( const product of cart){
-        totalPrice = totalPrice + product.price
+        // product.quantity = product.quantity || 1; /* if pq is 0 then set pq = 1 */
+        totalPrice = totalPrice + product.price * product.quantity
         totalShipping = totalShipping + product.shipping
+        quantity = quantity + product.quantity
     }
     const tax = totalPrice*7/100;
     const grandTotal = totalPrice + totalShipping + tax
     return (
         <div className='cart'>
                <h4>Order Summary</h4>
-                <p>Selected Items : {cart.length}</p>
+                <p>Selected Items : {quantity}</p>
                 <p>Total price : ${totalPrice}</p>
                 <p>Total shipping : ${totalShipping}</p>
                 <p>Tax : ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,20 +1,55 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([])
     const [cart, setCart] = useState([])
+
     useEffect(()=>{ /* annonymouse func which will run at any change */
-    fetch('products.json')
-    .then(res => res.json())
-    .then(data => setProducts(data)) /* data setProducts e set korle data jodi fetch hoy tahole products e pabo */
-    },[]) /* empty array for showing after loading component  */
-    
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data)) /* data setProducts e set korle data jodi fetch hoy tahole products e pabo */
+    },[]) /* empty array for showing after loading component  */ 
+
+    useEffect(()=>{
+        const storedCart = getShoppingCart()
+        const savedCart =[]
+        // step1  get id of the stored product
+        for(const id in storedCart){
+            //step2 get the product by using id
+            const addedProduct = products.find(product=> product.id === id)
+            //step 3 get quantity of the product
+            if(addedProduct){
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity
+                // step4 add the addedProduct to savedCart
+                savedCart.push(addedProduct)
+                // console.log(addedProduct)
+            }
+            // step5 set the cart
+            setCart(savedCart) /* after loop ends */
+        }
+    },[products])
+
     const handleAddToCart =(product)=>{
         // console.log('added product', product.name)
-        const newCart = [...cart, product]
+        let newCart =[]
+        // const newCart = [...cart, product]
+        // if product doesnt exist in cart then set quantity = 1
+        // if exist update by 1
+        const exists = cart.find(pd => pd.id === product.id)
+        if(!exists){
+            product.quantity = 1
+            newCart = [...cart, product]
+        }else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id)
+            newCart = [...remaining, exists]
+        }
         setCart(newCart)
+        addToDb(product.id)
     }
 
     return (
